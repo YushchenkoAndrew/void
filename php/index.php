1f144504a1842315116bf6f27aa45e96be6760b4
Yushchenko Andrew <Andrew10122000@gmail.com>
2021-10-17 02:09:40 +0300

    Add simple Ping/Pong route on HEAD

--- php/index.php
+++ php/index.php
@@ -2,6 +2,7 @@
 
 // Simple routing option
 switch ($_SERVER["REQUEST_METHOD"]) {
+  case "HEAD": return include_once("./ping.php");
   case "GET": return include_once("./read.php");
   case "POST": return include_once("./upload.php");
   case "DELETE": return include_once("./delete.php");
