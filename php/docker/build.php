2aac290cadf3783f248ec1a8bf396c4e4c732a71
Andrew Y <Andrew10122000@gmail.com>
2022-04-05 18:45:14 +0300

    Fix some bugs with output stream buffering

--- php/docker/build.php
+++ php/docker/build.php
@@ -17,7 +17,19 @@ $p = new PharData(TAR_FILE);
 $p->buildFromDirectory($path);
 $p->compress(Phar::GZ);
 
-$ch = curl_init(getenv("DOCKER_URL") . "/build?" . http_build_query(["t" => $_GET["t"]]));
+$ch = curl_init(getenv("DOCKER_URL") . "/build?" . http_build_query([
+  // A name and optional tag to apply to the image in the name:tag format.
+  // If you omit the tag the default latest value is assumed.
+  // You can provide several t parameters.
+  "t" => $_GET["t"],
+
+  // Do not use the cache when building the image.
+  "nocache" => true,
+
+  // Remove intermediate containers after a successful build.
+  "rm" => true,
+]));
+
 curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/tar"]);
 
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -30,6 +42,18 @@ curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($curl, $data) {
   print(($res = json_decode($data ?? "")) ? $res->stream : "");
   return strlen($data);
 });
+
+
+// This header will disable fastcgi_buffering and gzip
+header("X-Accel-Buffering: no");
+header("Cache-Control: no-cache, must-revalidate");
+
+// Set unlimited execution time
+set_time_limit(0); 
+
+// Send buffer ctx immediately and delete topmost buffer
+ob_implicit_flush();
+ob_end_flush();
 
 $res = curl_exec($ch);
 $err = curl_error($ch);
